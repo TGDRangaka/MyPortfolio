00e7870322f93ea6f69c8efaacd3850b6e334633
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-08-14 23:17:56 +0530

    feat: send email

--- src/componets/Form.tsx
+++ src/componets/Form.tsx
@@ -1,15 +1,51 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Form() {
+  const inputStyle = 'w-full rounded px-3 py-2 text-sm bg-grey  lg:text-base'
+  const form = useRef<HTMLFormElement>(null);
+  
+  const [isSubmit, setSubmit] = useState(false);
+  const [isSubmiting, setSubmiting] = useState(false);
 
-    const inputStyle = 'w-full rounded px-3 py-2 text-sm bg-grey  lg:text-base'
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmiting(true);
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID as string,
+        import.meta.env.VITE_TEMPLATE_ID as string,
+        form.current as string | HTMLFormElement,
+        {
+          publicKey: import.meta.env.VITE_PUBLIC_KEY as string,
+        })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setSubmit(true);
+          setSubmiting(false);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setSubmiting(false);
+          setSubmit(false);
+          alert('Failed to send email. Please try again.');
+        },
+      );
+  };
 
   return (
-    <form className="flex flex-col gap-2">
-        <input className={`${inputStyle} `} type="text" name="name" placeholder="Name" id="" />
-        <input className={`${inputStyle} `} type="email" name="email" placeholder="Email" id="" />
-        <input className={`${inputStyle} `} type="text" name="subject" placeholder="Subject" id="" />
-        <textarea className={`${inputStyle} h-20`} name="message" placeholder="Message" id=""></textarea>
-        <button className=" py-2 text-black font-bold text-lg rounded-sm bg-green lg:text-2xl hover:scale-110 duration-150" type="submit">SUBMIT</button>
+    <form className="flex flex-col gap-2" ref={form} onSubmit={sendEmail}>
+      <input className={`${inputStyle} `} type="text" name="name" placeholder="Name" id="" />
+      <input className={`${inputStyle} `} type="email" name="email" placeholder="Email" id="" />
+      <input className={`${inputStyle} `} type="text" name="subject" placeholder="Subject" id="" />
+      <textarea className={`${inputStyle} h-20`} name="message" placeholder="Message" id=""></textarea>
+      <button className=" py-2 text-black font-bold text-lg rounded-sm bg-green lg:text-2xl hover:scale-110 duration-150" type="submit">
+        {
+          isSubmiting ? <span className='animate-pulse text-black'>Submitting...</span>: isSubmit? 'Submitted!' : 'Submit'
+        }
+      </button>
     </form>
   )
 }
